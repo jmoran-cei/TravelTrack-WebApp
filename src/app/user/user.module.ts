1a90faad5994531122ace7125a367329e01caca6
Jon Moran <jmoran@ceiamerica.com>
2022-07-13 13:36:48 -0400

    cleaned /user imports (forgot to when creating index files)

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { EditProfileComponent } from "./edit-profile.component";
-import { SettingsComponent } from "./settings.component";
-import { UserService } from "./shared/user.service";
-import { userRoutes } from "./user.routes";
+
+import {
+  EditProfileComponent,
+  SettingsComponent,
+  UserService,
+  userRoutes
+} from './index'
 
 @NgModule({
   imports: [
