9fda27a4174acd52e94d267226d106a6c0da1719
Jon Moran <jmoran@ceiamerica.com>
2022-08-10 09:57:19 -0400

    in-mem-web-api installed and data service made

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,141 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService implements InMemoryDbService {
+  constructor() { }
+
+  createDb() {
+    return {
+
+      trips: [
+        {
+          id: 1,
+          details: '',
+          title: 'Brothers\' Anguila Trip',
+          startDate: new Date('3/15/2022'),
+          endDate: new Date('3/20/2022'),
+          destinations: [
+            {
+              destinationId: 1,
+              city: 'Zemi Beach',
+              stateProv: 'West End',
+              country: 'Anguilla',
+            }
+          ],
+          members: [],
+          photos: [],
+          itinerary: [],
+          toDo: [],
+          imgUrl: "assets/images/trips/anguila1.jpg"
+
+        }, {
+          id: 2,
+          title: 'Myrtle Beach and Charleston Family Vacay 2022',
+          details: '',
+          startDate: new Date('5/27/2022'),
+          endDate: new Date('6/5/2022'),
+          destinations: [
+            { destinationId: 1,
+              city: 'Myrtle Beach',
+              stateProv: 'South Carolina',
+              country: 'United States'
+            },
+            {
+              destinationId: 2,
+              city: 'Charleston',
+              stateProv: 'South Carolina',
+              country: 'United States'
+            },
+          ],
+          members: [],
+          photos: [],
+          itinerary: [],
+          toDo: [],
+          imgUrl: "assets/images/trips/myrtlebeach1.jpg"
+
+        }, {
+          id: 3,
+          title: 'Smoky Mountains Thanksgiving',
+          details: '',
+          startDate: new Date('11/23/2019'),
+          endDate: new Date('11/30/2019'),
+          destinations: [
+            {
+              destinationId: 1,
+              city: 'Gatlinburg',
+              stateProv: 'Tennessee',
+              country: 'United States'
+            }
+          ],
+          members: [],
+          photos: [],
+          itinerary: [],
+          toDo: [],
+          imgUrl: "assets/images/trips/gatlinburg1.jpg"
+        },{
+          id: 4,
+          title: 'Hawaii Family Trip 2023',
+          details: '',
+          startDate: new Date('6/24/2023'),
+          endDate: new Date('7/5/2023'),
+          destinations: [
+            { destinationId: 1,
+              city: 'Maui',
+              stateProv: 'Hawaii',
+              country: 'United States'
+            },
+          ],
+          members: [],
+          photos: [],
+          itinerary: [],
+          toDo: [],
+          imgUrl: "assets/images/trips/hawaii1.jpg"
+
+        },{
+          id: 5,
+          title: 'Our Ireland Trip',
+          details: '',
+          startDate: new Date('3/11/2023'),
+          endDate: new Date('3/20/2023'),
+          destinations: [
+            { destinationId: 1,
+              city: 'Dublin',
+              stateProv: 'Lienster',
+              country: 'Ireland'
+            },
+          ],
+          members: [],
+          photos: [],
+          itinerary: [],
+          toDo: [],
+          imgUrl: "assets/images/trips/ireland1.jpg"
+
+        }
+      ],
+
+      users: [
+        {
+          username: 'jmoran@ceiamerica.com',
+          password: 'P@ssw0rd',
+          firstName: 'Jonathan',
+          lastName: 'Moran',
+          address: [],
+          pictureURL: 'assets/images/dummy1.jpg'
+        },
+        {
+          username: 'dummyuser@dummy.dum',
+          password: 'P@ssw0rd',
+          firstName: 'Dummy',
+          lastName: 'User',
+          address: [],
+          pictureURL: 'assets/images/users/dummy1.jpg'
+        }
+      ]
+
+    };
+  }
+}
