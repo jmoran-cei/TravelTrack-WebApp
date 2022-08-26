e2eff9ce0dd89de67647ee18d7e56e65eb84e36d
Jon Moran <jmoran@ceiamerica.com>
2022-08-26 09:47:54 -0400

    trip resolver now utilizes take(1), better formatting for trip service file

--- src/app/trips/trip-overview/trip-resolver.component.ts
+++ src/app/trips/trip-overview/trip-resolver.component.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { TripService } from '../shared';
+import { take } from 'rxjs';
 
 @Injectable()
 export class TripResolver implements Resolve<any> {
   constructor(private tripService: TripService, private router: Router) {}
 
   resolve(route: ActivatedRouteSnapshot) {
-    this.tripService.getTrip(route.params['id']).subscribe((response) => {
-      if (!response) this.router.navigate(['404']); // if it trip doesn't exist, re-route to 404
-    });
+    this.tripService.getTrip(route.params['id'])
+      .pipe(take(1))
+      .subscribe((response) => {
+        if (!response) this.router.navigate(['404']); // if it trip doesn't exist, re-route to 404
+      });
 
     return this.tripService.getTrip(route.params['id']);
   }
