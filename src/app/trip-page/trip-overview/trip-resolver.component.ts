5e6466ef8bbc2011d1fb0a23a6107a7e4caecba6
Jon Moran <jmoran@ceiamerica.com>
2022-09-14 16:34:31 -0400

    implementation changes: - only trips that the user is a part of are retrieved/displayed on user's trip list - trip resolver:  allows user to navigate to a trip path (trips/id) only if they are a member of it and it exists

--- src/app/trip-page/trip-overview/trip-resolver.component.ts
+++ src/app/trip-page/trip-overview/trip-resolver.component.ts
@@ -2,16 +2,34 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { TripService } from '../../trips/.';
 import { take } from 'rxjs';
+import { ITrip } from 'src/app/shared';
+import { AuthService } from 'src/app/user';
 
 @Injectable()
 export class TripResolver implements Resolve<any> {
-  constructor(private tripService: TripService, private router: Router) {}
+  constructor(
+    private tripService: TripService,
+    private router: Router,
+    private auth: AuthService
+  ) {}
+
+  // verify if the user is a member of the trip
+  checkUserIsMemberOfTrip(trip: ITrip): boolean {
+    for (let member of trip.members) {
+      if (member.username === this.auth.currentUser.username) return true;
+    }
+
+    return false;
+  }
 
   resolve(route: ActivatedRouteSnapshot) {
-    this.tripService.getTrip(route.params['id'])
+    this.tripService
+      .getTrip(route.params['id'])
       .pipe(take(1))
       .subscribe((response) => {
-        if (!response) this.router.navigate(['404']); // if trip doesn't exist, re-route to 404
+        if (!response || !this.checkUserIsMemberOfTrip(response)){
+          this.router.navigate(['404']); // if trip doesn't exist, re-route to 404
+        }
       });
 
     return this.tripService.getTrip(route.params['id']);
