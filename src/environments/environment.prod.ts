539277a310192d4248f17f91b29b2ae94cfaf809
Jon Moran <jmoran@ceiamerica.com>
2023-01-24 08:37:23 -0500

    prod environment changed to read env variables added into YAML, retesting deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  TravelTrackAPI: "https://bootcamp-traveltrackapi.azurewebsites.net",
-  TravelTrackAPIKey: "SJQBxlXwsyI3BvUMCFrg6CnoBz8PaVus", // not normal practice (also relying on CORS to keep API secure from intruders)
-  GooglePlacesAPIKey: "AIzaSyAHFf5atq9RqiJZ41TrepX47Ub2_G_Wm00"
+  TravelTrackAPI: `${process.env?.['TRAVELTRACK_API_BASE_URL']}`,
+  TravelTrackAPIKey: `${process.env?.['TRAVELTRACK_API_KEY']}`,
+  GooglePlacesAPIKey: `${process.env?.['GOOGLE_PLACES_API_KEY']}`
 };
