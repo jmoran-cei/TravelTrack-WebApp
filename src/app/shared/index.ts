cfdd52c6c6f3208341ce21a487cf5b27b978277e
Jon Moran <jmoran@ceiamerica.com>
2023-01-25 17:31:51 -0500

    add TripPhoto model, update trip implementations

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,6 +1,10 @@
+// services
 export * from './navigation/navigation.service';
+export * from './services/trip-photo.service'
 
 // models
 export * from './models/trip.model';
 export * from './models/user.model';
 export * from './models/destination.model';
+export * from './models/toDo.model';
+export * from './models/tripPhoto.model';
---
+++ src/app/shared/models/tripPhoto.model.ts
@@ -0,0 +1,9 @@
+export interface TripPhoto {
+  id: string;
+  tripId: string;
+  fileName: string;
+  fileType: string;
+  path: string;
+  addedByUser: string;
+  alt: string;
+}
